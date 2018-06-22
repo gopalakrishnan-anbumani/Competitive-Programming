4bf5901390f096328bdd02dc15c2559cbc6b6d43
Gopal <gopalakrishnanbe16@gmail.com>
2018-06-22 08:23:09 +0530

    - Level Order Search BST - Delete node from tree

--- src/datastructure/tree/BinarySearchTre.java
+++ src/datastructure/tree/BinarySearchTre.java
@@ -1,5 +1,8 @@
 package datastructure.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTre {
 	
 	int data;
@@ -52,11 +55,44 @@ public class BinarySearchTre {
 		}
 	}
 	
+	
+	//=====================================================
+	// DELETE
+	//=====================================================
+	
 	public void deleteNode(int value){
-		if(data > value){
-			
-		}
+		deleteNode(this,value);
 	}
+	private BinarySearchTre deleteNode(BinarySearchTre node, int value){
+		if(node == null){
+			System.out.println("Node is Empty");
+			return null;
+		}
+		
+		if(node.data > value){
+			node.left = deleteNode(node.left, value);
+		}else if(node.data < value){
+			node.right = deleteNode(node.right, value);
+		}else{
+			
+			if(node.left == null) return node.right;
+			else if(node.right == null) return node.left;
+			else{
+				node.data = retrieveFromRightMost(node.left);
+				node.left = deleteNode(node.left, node.data);
+			}
+		}
+	return node;
+	}
+	private int retrieveFromRightMost(BinarySearchTre node){
+		
+		while(node.right!=null){
+			node = node.right;
+		}
+		
+		return node.data;
+	}
+
 	
 	/**
 	 * Main Method
@@ -65,11 +101,19 @@ public class BinarySearchTre {
 		BinarySearchTre tree = new BinarySearchTre(20);
 		tree.addNode(10);
 		tree.addNode(25);
-		tree.addNode(4);
-		tree.addNode(5);
+		tree.addNode(11);
+		tree.addNode(9);
+		tree.addNode(30);
+		tree.addNode(22);
+		tree.addNode(50);
 		tree.addNode(6);
+//		tree.addNode(1);
 		//preOrderTraversal(tree);
-		System.out.println(tree.contains(40));
+		//System.out.println(tree.contains(40));
+		//tree.deleteNode(215);
+		System.out.println();
+		//preOrderTraversal(tree);
+		levelOrderSearch(tree);
 		
 	}
 	public static void preOrderTraversal(BinarySearchTre node){
@@ -77,5 +121,22 @@ public class BinarySearchTre {
 		System.out.print(" *"+node.data+"*");
 		preOrderTraversal(node.left);
 		preOrderTraversal(node.right);
+	}
+	//=========================================
+	// LEVEL ORDER SEARCH - BST
+	//=========================================
+	public static void levelOrderSearch(BinarySearchTre tree){
+		Queue<BinarySearchTre> queue = new LinkedList<>();
+		queue.add(tree);
+		while(!queue.isEmpty()){
+			BinarySearchTre node = queue.remove();
+			System.out.print(node.data+" ");
+			if(node.left !=null){
+				queue.add(node.left);
+			}
+			if(node.right != null){
+				queue.add(node.right);
+			}
+		}
 	}
 }
