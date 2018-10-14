0ba730324b344024104e4f371096f0fdd2abdb3b
Gopal <gopalakrishnanbe16@gmail.com>
2018-10-14 05:56:52 +0530

    - DP sum

--- src/code/java/hackerrank/AllHackerRankQues.java
+++ src/code/java/hackerrank/AllHackerRankQues.java
@@ -1,5 +1,8 @@
 package code.java.hackerrank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class AllHackerRankQues {
@@ -82,21 +85,85 @@ public class AllHackerRankQues {
 		return false;
 	}
 
-    private static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int q = scan.nextInt();
-        while (q-- > 0) {
-            int n = scan.nextInt();
-            int leap = scan.nextInt();
-            
-            int[] game = new int[n];
-            for (int i = 0; i < n; i++) {
-                game[i] = scan.nextInt();
-            }
+	
+	private static void findMinimumPathSum(){
+		
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		List<Integer> row1 = new ArrayList<Integer>(Arrays.asList(2));
+		List<Integer> row2 = new ArrayList<Integer>(Arrays.asList(3,4));
+		List<Integer> row3 = new ArrayList<Integer>(Arrays.asList(5,6,7));
+		List<Integer> row4 = new ArrayList<Integer>(Arrays.asList(4,2,8,1));
+		list.add(row1); list.add(row2); list.add(row3); list.add(row4);
+		printListOfListIntegers(list);
+		MaxPath(list);
+		
+	}
+	
+	public static int MaxPath(List<List<Integer>> list){
+		
+		if(list==null || list.size() == 0){
+			return 0;
+		}
+		if(list.size()==1){
+			int max = list.get(0).get(0);
+			for(int a : list.get(0)){
+				if(max<a){
+					max=a;
+				}
+			}
+			return max;
+		}
+		
+		for(int currentRow=1;currentRow<list.size();currentRow++){
+			int lastIndex = list.get(currentRow).size();
+			for(int index = 0; index<lastIndex; index++){
+				int index1 = (index-1 >= 0) ? index-1 : 0;    
+				int index2 = index >= list.get(currentRow-1).size() ? index-1 : index;
+				int index1Num = list.get(currentRow-1).get(index1);
+				int index2Num = list.get(currentRow-1).get(index2);
+				int currentIndexNum = list.get(currentRow).get(index);
+				int maxNum = (currentIndexNum+index1Num) < (currentIndexNum+index2Num) ? currentIndexNum+index1Num : currentIndexNum+index2Num;
+				list.get(currentRow).set(index, maxNum);
+			}
+		}
+		
+		printListOfListIntegers(list);
+			
+		
+		return 0;
+	}
 
-            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
-        }
-        scan.close();
+	/**
+	 * Print List of List Integers
+	 *
+	 * @param list
+	 */
+	private static void printListOfListIntegers(List<List<Integer>> list) {
+		for(List<Integer> l : list){
+			for(int a : l){
+				System.out.print(a + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int q = scan.nextInt();
+//        while (q-- > 0) {
+//            int n = scan.nextInt();
+//            int leap = scan.nextInt();
+//            
+//            int[] game = new int[n];
+//            for (int i = 0; i < n; i++) {
+//                game[i] = scan.nextInt();
+//            }
+//
+//            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+//        }
+//        scan.close();
+    	
+    	findMinimumPathSum();
     }
 
 }
